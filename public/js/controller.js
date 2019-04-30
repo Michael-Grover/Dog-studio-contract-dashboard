6d64da1f274137dd8bd5790062d30adfab409dc2
MIchael Grover <michael@clause.io>
2019-04-30 16:23:58 -0400

    adapted code to milestone Clause

--- public/js/controller.js
+++ public/js/controller.js
@@ -1,60 +1,33 @@
 'use strict';
 
-/* eslint-disable no-undef */
-/* eslint-disable no-unused-vars */
+/* eslint-enable no-undef */
+/* eslint-enable no-unused-vars */
 
-let max = 0, min = 0, payOut = 0;
-let accX = 0.0, accY = 0.0, accZ = 0.0;
-let running = false;
 
-const timeLimit = 10; // seconds
-
-// The payload that gets sent to the smart contact
 let body = {
-  '$class': 'io.clause.demo.fragileGoods.DeliveryUpdate',
-  'startTime':'',
-  'finishTime':'',
-  'status':'IN_TRANSIT',
-  'accelerometerReadings':[] // Readings accumulate in here
+  '$class': 'org.accordproject.payment.milestone.MilestoneReached',
+  'milestoneName':'milestone1',
 };
 
-$('#cost').hide();
-$('#costValue').hide();
-$('.collapseExample').collapse('show');
-
 /**
- * Updates the Accelerometer values on the page
+ * Approve milestone 2
  */
-function updatePage(){
-  $('#accX').html(accX);
-  $('#accY').html(accY);
-  $('#accZ').html(accZ);
+function milestone2() {
+  body.milestone = 'milestone 1';
+  $('#milestone3Button').removeAttr('disabled');
+  $('#milestone2Button').attr('disabled', 'disabled');
+
+  /*sendEvent();*/
 }
 
-if (window.DeviceMotionEvent !== undefined) {
-  /**
-   * Tracks the maximum acceleration between each submission to the smart contact.
-   * @param {*} e the DeviceMotionEvent
-   */
-  window.ondevicemotion = function(e) {
+/**
+ * Approve milestone 3
+ */
+function milestone3() {
+  body.milestone = 'milestone 2';
+  $('#milestone3Button').attr('disabled', 'disabled');
 
-    if(e.acceleration.x > max) { max = e.acceleration.x;}
-    if(e.acceleration.y > max) { max = e.acceleration.y;}
-    if(e.acceleration.z > max) { max = e.acceleration.z;}
-    if(e.acceleration.x < min) { min = e.acceleration.x;}
-    if(e.acceleration.y < min) { min = e.acceleration.y;}
-    if(e.acceleration.z < min) { min = e.acceleration.z;}
-
-    if(e.acceleration.x && e.acceleration.y && e.acceleration.z){
-      accX = Number.parseFloat(Math.abs(e.acceleration.x)).toFixed(1);
-      accY = Number.parseFloat(Math.abs(e.acceleration.y)).toFixed(1);
-      accZ = Number.parseFloat(Math.abs(e.acceleration.z)).toFixed(1);
-    }
-
-    updatePage();
-  };
-} else {
-  // TODO Add a notice to the page that this demo won't work on this device
+  sendEvent();
 }
 
 /**
@@ -62,8 +35,7 @@ if (window.DeviceMotionEvent !== undefined) {
  *
  * @param {function} callback A callback function to invoke after the event has been sent
  */
-function sendEvent(callback){
-  body.finishTime = new Date();
+function sendEvent(){
   let tempBody = body;
   $.ajax({
     type: 'POST',
@@ -73,92 +45,5 @@ function sendEvent(callback){
       'Content-Type': 'application/json'
     },
     data: JSON.stringify(tempBody)
-  }).then(function(data){
-    payOut = (data.amount.doubleValue)? data.amount.doubleValue: data.amount;
-    updatePage();
-    if(callback){ callback(payOut); }
-  });
+  })
 }
-
-/**
- * Start the count-down timer
- */
-function start() {
-  body.startTime = new Date();
-  body.finishTime = new Date();
-  $('#startButton').attr('disabled', 'disabled');
-  $('#finishButton').removeAttr('disabled');
-  $('#cost').hide();
-  running = true;
-}
-
-/**
- * Stop the count-down timer.
- */
-function finish() {
-
-  $('#cost').show();
-  $('#costSpinner').show();
-
-  running = false;
-  body.finishTime = new Date();
-  body.status = 'ARRIVED';
-  $('#startButton').removeAttr('disabled');
-  $('#finishButton').attr('disabled', 'disabled');
-
-  sendEvent(function(payOut){
-    $('#costValue').show();
-    $('#costSpinner').hide();
-    $('#payOut').html(payOut);
-  });
-}
-
-/**
- * Reset the page so that we can run the demo again.
- */
-function reset() {
-  body.startTime = new Date();
-  running = false;
-  max = 0, min = 0, payOut = 0;
-  body = {
-    '$class': 'io.clause.demo.fragileGoods.DeliveryUpdate',
-    'startTime':'',
-    'finishTime':'',
-    'status':'IN_TRANSIT',
-    'accelerometerReadings':[]
-  };
-  $('#cost').hide();
-  $('#costValue').hide();
-  $('#timer').html(timeLimit);
-  $('#payOut').html(0.0);
-}
-
-/**
- * Send an accelerometer reading every 2.5 secs.
- */
-setInterval(function(){
-  if(running){
-    body.accelerometerReadings.push(max);
-    body.accelerometerReadings.push(min);
-
-    min = 0;
-    max = 0;
-
-    sendEvent();
-  }
-},2500);
-
-/**
- * Update the timer on the page.
- */
-setInterval(function(){
-  let now = new Date();
-  let timeLeft =Number.parseFloat((timeLimit-((now.getTime() - new Date(body.startTime).getTime())/1000))).toFixed(1);
-  if(running){
-    $('#timer').html(timeLeft);
-  }
-
-},100);
-
-/* eslint-enable no-undef */
-/* eslint-enable no-unused-vars */
